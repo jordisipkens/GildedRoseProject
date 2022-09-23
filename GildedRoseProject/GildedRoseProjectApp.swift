542cee0646bf58648f9d2f2143b9e5de82345841
Jordi Sipkens <jordisipkens@users.noreply.github.com>
2022-09-23 11:38:12 +0200

    Initial Commit

---
+++ GildedRoseProject/GildedRoseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GildedRoseProjectApp.swift
+//  GildedRoseProject
+//
+//  Created by Jordi Sipkens on 23/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct GildedRoseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
